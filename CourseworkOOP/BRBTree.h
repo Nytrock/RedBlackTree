1acc076abc0269284a5613dc994d665e2fe1d0a4
Nytrock <savvazhirnov@gmail.com>
2025-04-02 23:06:20 +0300

    Добавьте файлы проекта.

---
+++ CourseworkOOP/BRBTree.h
@@ -0,0 +1,50 @@
+#ifndef __BRBTREE_H__
+#define __BRBTREE_H__
+namespace std {
+	enum class Color {
+		Red,
+		Black
+	};
+
+	template <typename T> class Node {
+	private:
+		T _value;
+		Node<T>* _leftChild;
+		Node<T>* _rightChild;
+		Node<T>* _parent;
+		Color _color;
+	public:
+		template<T> Node(T value, Node<T>* parent) {
+			_value = value;
+			_leftChild = nullptr;
+			_rightChild = nullptr;
+			_parent = parent;
+			_color = Color::Red;
+		}
+
+		void changeColor(Color color);
+		T getValue();
+		Color getColor();
+		Node<T>* getLeftChild();
+		Node<T>* getRightChild();
+		Node<T>* getParent();
+		void takePlaceOfParent();
+	};
+
+	template <typename T> class BRBTree {
+	private:
+		Node<T>* _root;
+		void balanceTreeAfterInsert();
+		void balanceTreeAfterErase();
+		T getMax(Node<T>* node);
+		T getMin(Node<T>* node);
+	public:
+		void insert(T value);
+		void erase(T value);
+		void erase(Node<T>* node);
+		T getMax();
+		T getMin();
+		Node<T>* find(T value);
+	};
+}
+#endif
---
+++ CourseworkOOP/CourseworkOOP.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+#include "BRBTree.cpp"
+
+using namespace std;
+
+int main() {
+    setlocale(LC_CTYPE, "Russian_Russia.1251");
+
+    BRBTree<int> tree;
+    tree.insert(1);
+
+    system("pause");
+}
