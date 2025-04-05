acd89ce440264b4d2bc919ba000e8bf0dab025ef
Nytrock <savvazhirnov@gmail.com>
2025-04-05 10:54:13 +0300

    Add balancing

--- CourseworkOOP/BRBTree.h
+++ CourseworkOOP/BRBTree.h
@@ -44,6 +44,8 @@ namespace std {
 
 		void setLeftChild(Node<T>* leftChild) {
 			_leftChild = leftChild;
+			if (_leftChild != nullptr)
+				_leftChild->setParent(this);
 		}
 
 		Node<T>* getRightChild() {
@@ -52,6 +54,8 @@ namespace std {
 
 		void setRightChild(Node<T>* rightChild) {
 			_rightChild = rightChild;
+			if (_rightChild != nullptr)
+				_rightChild->setParent(this);
 		}
 
 		Node<T>* getParent() {
@@ -63,7 +67,24 @@ namespace std {
 		}
 
 		void takePlaceOfParent() {
+			Node<T>* parent = _parent;
+			Node<T>* grandparent = _parent->getParent();
 
+			_parent = grandparent;
+			if (grandparent != nullptr) {
+				if (_value < grandparent->getValue())
+					grandparent->setLeftChild(this);
+				else
+					grandparent->setRightChild(this);
+			}
+
+			if (_value >= parent->getValue()) {
+				parent->setRightChild(_leftChild);
+				setLeftChild(parent);
+			} else {
+				parent->setLeftChild(_rightChild);
+				setRightChild(parent);
+			}
 		}
 
 		Node<T>* getMaxNode() {
@@ -79,7 +100,12 @@ namespace std {
 		}
 
 		void print() {
-			cout << _value << " ";
+			cout << _value;
+			if (_color == Color::Red)
+				cout << "R ";
+			else
+				cout << "B ";
+
 			if (_leftChild != nullptr)
 				_leftChild->print();
 			if (_rightChild != nullptr)
@@ -91,12 +117,90 @@ namespace std {
 	private:
 		Node<T>* _root = nullptr;
 
-		void balanceTreeAfterInsert(Node<T>* newNode) {
+		void switchNodeAndHisParent(Node<T>* node) {
+			bool isParentRoot = node->getParent() == _root;
+			node->takePlaceOfParent();
+			if (isParentRoot)
+				_root = node;
+		}
 
+		void balanceTreeAfterInsert(Node<T>* newNode) {
+			if (newNode == _root) {
+				newNode->setColor(Color::Black);
+				return;
+			}
+
+			Node<T>* parent = newNode->getParent();
+			Node<T>* grandparent = parent->getParent();
+
+			if (grandparent == nullptr)
+				return;
+
+			Node<T>* uncle;
+			if (parent == grandparent->getLeftChild())
+				uncle = grandparent->getRightChild();
+			else
+				uncle = grandparent->getLeftChild();
+
+			Color uncleColor = Color::Black;
+			if (uncle != nullptr)
+				uncleColor = uncle->getColor();
+
+			if (parent->getColor() == Color::Red && uncleColor == Color::Red) {
+				parent->setColor(Color::Black);
+				uncle->setColor(Color::Black);
+				if (grandparent != _root)
+					grandparent->setColor(Color::Red);
+			} else if (parent->getColor() == Color::Red) {
+				if ((parent->getRightChild() == newNode) != (grandparent->getRightChild() == parent)) {
+					switchNodeAndHisParent(newNode);
+					parent = newNode;
+					grandparent = parent->getParent();
+				}
+				parent->setColor(Color::Black);
+				grandparent->setColor(Color::Red);
+				switchNodeAndHisParent(parent);
+			}
 		}
 
 		void balanceTreeAfterErase(Node<T>* erasedNodeBrother) {
+			Node<T>* brotherLeftChild = erasedNodeBrother->getLeftChild();
+			Node<T>* brotherRightChild = erasedNodeBrother->getRightChild();
+			Node<T>* parent = erasedNodeBrother->getParent();
 
+			if (erasedNodeBrother->getColor() == Color::Black) {
+				if (brotherRightChild->getColor() == Color::Red) {
+					erasedNodeBrother->setColor(parent->getColor());
+					brotherRightChild->setColor(Color::Black);
+					parent->setColor(Color::Black);
+					switchNodeAndHisParent(erasedNodeBrother);
+					return;
+				}
+
+				if (brotherLeftChild->getColor() == Color::Red) {
+					erasedNodeBrother->setColor(Color::Red);
+					brotherLeftChild->setColor(Color::Black);
+					switchNodeAndHisParent(brotherLeftChild);
+					return;
+				}
+
+				if (parent->getColor() == Color::Black) {
+					if (parent == _root) {
+						erasedNodeBrother->setColor(Color::Red);
+						return;
+					}
+
+					parent->setColor(Color::Red);
+					switchNodeAndHisParent(erasedNodeBrother);
+				}
+
+				erasedNodeBrother->setColor(Color::Red);
+				parent->setColor(Color::Black);
+			} else {
+				erasedNodeBrother->setColor(Color::Black);
+				parent->setColor(Color::Red);
+				switchNodeAndHisParent(erasedNodeBrother);
+			}
 		}
 	public:
 		void print() {
@@ -107,6 +211,7 @@ namespace std {
 		void insert(T value) {
 			if (_root == nullptr) {
 				_root = new Node<T>(value, nullptr);
+				balanceTreeAfterInsert(_root);
 				return;
 			}
 
@@ -115,14 +220,14 @@ namespace std {
 
 			while (currentNode != nullptr) {
 				parent = currentNode;
-				if (value > currentNode->getValue())
+				if (value >= currentNode->getValue())
 					currentNode = currentNode->getRightChild();
 				else
 					currentNode = currentNode->getLeftChild();
 			}
 
 			Node<T>* newNode = new Node<T>(value, parent);
-			if (value > parent->getValue())
+			if (value >= parent->getValue())
 				parent->setRightChild(newNode);
 			else
 				parent->setLeftChild(newNode);
@@ -165,9 +270,9 @@ namespace std {
 			} 
 			
 			if (leftChild != nullptr && rightChild != nullptr) {
-				Node<T>* minByRight = rightChild->getMinNode();
-				T value = minByRight->getValue();
-				erase(minByRight);
+				Node<T>* nodeToReplace = leftChild->getMaxNode();
+				T value = nodeToReplace->getValue();
+				erase(nodeToReplace);
 				node->setValue(value);
 				return;
 			} 
--- CourseworkOOP/CourseworkOOP.cpp
+++ CourseworkOOP/CourseworkOOP.cpp
@@ -7,18 +7,9 @@ int main() {
     setlocale(LC_CTYPE, "Russian_Russia.1251");
 
     BRBTree<int> tree;
-    tree.insert(7);
     tree.insert(8);
-    tree.insert(5);
-    tree.insert(9);
-    tree.insert(3);
     tree.insert(6);
-    tree.insert(4);
-
-    tree.print();
-    cout << tree.getMin() << " " << tree.getMax() << endl;
-
-    tree.erase(5);
+    tree.insert(7);
     tree.print();
 
     system("pause");
